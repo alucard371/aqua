202c697699da3c2f01eb290ec3850b920f8bf6f4
alucard371 <test8q@gmail.com>
2016-12-11 17:16:25 +0100

    custom alice faker options

--- src/AppBundle/Entity/Genus.php
+++ src/AppBundle/Entity/Genus.php
@@ -45,6 +45,11 @@ class Genus
     private $funFact;
 
     /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isPublished;
+
+    /**
      * @return mixed
      */
     public function getName()
@@ -111,6 +116,14 @@ class Genus
     public function getUpdatedAt()
     {
         return new \DateTime('-'.rand(0, 100).' days');
+    }
+
+    /**
+     * @param mixed $isPublished
+     */
+    public function setIsPublished($isPublished)
+    {
+        $this->isPublished = $isPublished;
     }
 
 
